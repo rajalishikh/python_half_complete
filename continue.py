5865fd065d2d54217c3165f8a9d9af6140f83c8d
unknown <nahidujjamanraj@gmail.com>
2024-07-28 13:03:24 +0600

    In this file I solve 3 different problem using range and for loop

---
+++ continue.py
@@ -0,0 +1,8 @@
+i=1
+while i<20:
+    if(i%2 ==0):
+        i=i+1
+        continue
+    
+    print(i)
+    i=i+1
---
+++ for.py
@@ -0,0 +1,8 @@
+list=(4,16,20,24,28,32,36,40,44,48)
+x=int(input('please gve me a integer number '))
+idx=0
+for i in list:
+    if(x==i):
+        print ('find the number',idx)
+        idx=idx+1
+   
