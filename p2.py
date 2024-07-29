ab68050524dd3c2e8ee0ac9fe7b6c2fb62687b2d
unknown <nahidujjamanraj@gmail.com>
2024-07-29 12:00:16 +0600

    In this program I solve two problem

---
+++ p2.py
@@ -0,0 +1,17 @@
+# wap to find the sum of first natural number using while 
+i=1
+sum=0
+while i<=10:
+   
+    sum=sum+i
+    i=i+1
+print(sum)
+
+# write a program to find the factorial number 
+n=int(input('please give me a int number ')) 
+factorial=1  
+for i in range(1,n+1):
+    factorial=factorial*i
+print('here is the factorial number ',factorial)
+
+       
