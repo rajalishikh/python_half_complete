cd9415c571291d3f992e0a2b627c062dbe3d2e86
unknown <nahidujjamanraj@gmail.com>
2024-08-14 10:49:56 +0600

    In this  program i solve a one more problem

--- more.py
+++ more.py
@@ -1,15 +1,28 @@
-# prints n to 1 backwards 
+#question prints n to 1 backwards 
 # def show(n):
 #     if (n == 0):
 #         return
 #     print(n)
 #     show(n-1)
 # show(5)
-# prints n to 1 forward 
-def show2(n2):
-    if( n2 == 6):
-        return 
-    print(n2)
-    show2(n2+1)
 
-show2(1)
+# question prints n to 1 forward 
+
+# def show2(n2):
+#     if( n2 == 6):
+#         return 1 
+#     print(n2)
+#     return  show2(n2+1)+n2
+
+# result=show2(1)
+# print(result)
+
+# question calculate factorial 
+
+def factorial(n):
+    if(n == 0 or n== 1):
+        return 1
+    return factorial(n-1)*n
+print(factorial(5))
+
+
--- recursive.py
+++ recursive.py
@@ -2,6 +2,9 @@ def recursive(n):
     if(n==0):
         return 0
     
-   
     return recursive(n-1)+n
-print(recursive(5))
+    
+    
+   
+    
+print(recursive(5)) 
