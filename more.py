b4686343156aff4ca8fcf339dffa9b48911fe862
unknown <nahidujjamanraj@gmail.com>
2024-08-14 11:05:47 +0600

    In program i solve 4 problems

--- more.py
+++ more.py
@@ -1,4 +1,4 @@
-#question prints n to 1 backwards 
+#question: prints n to 1 backwards 
 # def show(n):
 #     if (n == 0):
 #         return
@@ -6,7 +6,7 @@
 #     show(n-1)
 # show(5)
 
-# question prints n to 1 forward 
+# question :prints n to 1 forward 
 
 # def show2(n2):
 #     if( n2 == 6):
@@ -17,12 +17,21 @@
 # result=show2(1)
 # print(result)
 
-# question calculate factorial 
+# question :calculate factorial 
 
-def factorial(n):
-    if(n == 0 or n== 1):
-        return 1
-    return factorial(n-1)*n
-print(factorial(5))
+# def factorial(n):
+#     if(n == 0 or n== 1):
+#         return 1
+#     return factorial(n-1)*n
+# print(factorial(5))
+
+# question : print all elements in a list using recursive function 
+def list_name(list,idx=0):
+    if(idx == len(list)):
+        return
+    print(list[idx])
+    list_name(list,(idx+1))
+name=['raj','barat','yousufe']
+list_name(name,idx=0)
 
 
